0164f12062929aabe83e40f06f8b5f66a71721b9
yalublukotikov <yalublukotikov@yandex.ru>
2022-03-02 23:19:04 +0300

    fix path to html and css filex

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const SERVER_PORT = 3000
+
+
+const server = http.createServer((req, res) => {
+    const {url} = req;
+    console.log('request', url);
+
+    let fileName = url === '/' ? 'index.html' : url;
+
+    const extension = fileName.split('.').pop()
+    if (extension == 'js') {
+        fileName.concat('js/');
+    } else if (extension == 'css') {
+        fileName.concat('css/');
+    }
+    fs.readFile(`${__dirname}/../src/${fileName}`, (err, file) => {
+        if (err) {
+            console.log('error');
+            res.write('404 not found');
+            res.end();
+            return;
+        }
+
+        res.write(file)
+        res.end();
+    });
+});
+
+server.listen(SERVER_PORT)
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+
+const btn = document.getElementById('btn');
+    btn.addEventListener('click', () =>{
+    console.log('click');
+})
