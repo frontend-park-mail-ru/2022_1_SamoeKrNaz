e42355248a50082217f2e9f973f082e456dc1f16
yalublukotikov <yalublukotikov@yandex.ru>
2022-03-07 15:33:41 +0300

    Добавлена новые компоненты, проведена шаблонизация основной страницы, дописан скрипт для формирования новой страницы

---
+++ src/basePage/basePage.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import * as render from './basePage.templ.js';
+
+Handlebars.registerPartial('leftMenu', Handlebars.templates['leftMenu']);
+Handlebars.registerPartial('cap', Handlebars.templates['cap']);
+Handlebars.registerPartial('desk', Handlebars.templates['desk']);
+Handlebars.registerPartial('activeTask', Handlebars.templates['activeTask']);
+Handlebars.registerPartial('containerDesk', Handlebars.templates['containerDesk']);
+Handlebars.registerPartial('rightMenu', Handlebars.templates['rightMenu']);
+
+
+const tasks = {
+    tasks: [{deskTitle: 'Доска для важных дел', deskDescp: 'Тут будет подробное описания таблицы и то зачем она. Но тут текст будет длиннее, чтобы можно было посмотреть, как увеличится блок.', deskDate: '11 Декабря 2022 года'},
+    {deskTitle: 'Доска для важных дел', deskDescp: 'Тут будет подробное описания таблицы и то зачем она. Но тут текст будет длиннее, чтобы можно было посмотреть, как увеличится блок.', deskDate: '11 Декабря 2022 года'},
+    {deskTitle: 'Доска для важных дел', deskDescp: 'Тут будет подробное описания таблицы и то зачем она. Но тут текст будет длиннее, чтобы можно было посмотреть, как увеличится блок.', deskDate: '11 Декабря 2022 года'},
+    {deskTitle: 'Доска для важных дел', deskDescp: 'Чтобы можно было посмотреть, как увеличится блок.', deskDate: '11 Декабря 2022 года'},
+    {deskTitle: 'Доска для важных дел', deskDescp: 'Чтобы можно было посмотреть, как увеличится блок.', deskDate: '11 Декабря 2022 года'}],
+    activeTasks: [{activeTaskText: 'Важное дело номер раз', activeTaskDate: '11 декабря 2022 года'},
+        {activeTaskText: 'Важное дело номер два', activeTaskDate: '11 декабря 2022 года'},
+        {activeTaskText: 'Важное дело номер три', activeTaskDate: '11 декабря 2022 года'},
+        {activeTaskText: 'Важное дело номер четыре', activeTaskDate: '11 декабря 2022 года'}],
+};
+
+const basePage = Handlebars.templates.basePage;
+const html = basePage(tasks);
+
+document.body.innerHTML += html;
+
+document.getElementsByClassName('Toggle__block')[0].onclick = toggleMenu;
+document.getElementsByClassName('Toggle__block_blue')[0].onclick = toggleActiveTasks;
+
+function toggleMenu() {
+    document.getElementsByClassName("Header")[0].classList.toggle("Header_open");
+    document.getElementsByTagName("body")[0].classList.toggle("menu-open");
+    document.getElementById("Search-icon").classList.toggle("Toggle__icon_open");
+}
+
+function toggleActiveTasks() {
+    document.getElementsByTagName("body")[0].classList.toggle("active-tasks-open");
+    document.getElementsByClassName("Active-tasks")[0].classList.toggle("close");
+    document.getElementsByClassName("Main__cap")[0].classList.toggle("active-close");
+    document.getElementById("active-closer").classList.toggle("Toggle__icon_open");
+}
