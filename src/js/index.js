346493f0e5c86952279fca54eeee2f74f1728da3
yalublukotikov <yalublukotikov@yandex.ru>
2022-03-08 13:56:48 +0300

    Фикс index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,8 @@ import {Ajax} from "../modules/ajax.js";
 import {addError} from "../modules/errors.js";
 import {Messages} from "../constants/constants.js";
 
+const domainSize = 27;
+
 const configApp = {
     signup: {
         href: '/sighup',
@@ -21,25 +23,37 @@ const configApp = {
         openMethod: basePageRender,
     }
 };
-const getUrl = window.location.href.slice(22);
-const aj = new Ajax;
-if (getUrl === 'login' || getUrl === ''){
-    loginPageRender();
+const getUrl = window.location.href.slice(domainSize);
+if (getUrl === 'login'){
+    const aj = new Ajax;
+    aj.get({url: ''})
+        .then(r => {
+            if (r.status == 401){
+                loginPageRender();
+            }
+            if (r.status === 200) {
+                window.history.pushState("", "", 'http://89.208.199.114:3000/base');
+                basePageRender(r.responseText);
+            };
+        })
+        .catch(er => {
+        });
 } else if (getUrl === 'signup') {
     signupPageRender();
-} else if (getUrl === 'base') {
-    aj.get({url: '/login'})
+} else if (getUrl === 'base' || getUrl === '') {
+    const aj = new Ajax;
+    aj.get({url: ''})
         .then(r => {
             if (r.status === 401) {
                 loginPageRender();
+                window.history.pushState("", "", 'http://89.208.199.114:3000/login');
             }
             if (r.status === 200) {
                 aj.get({url: ''}).then(r => {
                     console.log(r.status);
                     if (r.status === 200) {
                         console.log(r);
-                        window.history.pushState("", "", 'http://89.208.199.114:3000/login');
-                        basePageRender(r);
+                        basePageRender(r.responseText);
                     }
                 })
             };
@@ -52,7 +66,7 @@ document.body.addEventListener('click', (e) => {
     const {target} = e;
     if (target instanceof HTMLAnchorElement) {
         e.preventDefault();
-        const section = target.href.slice(22);
+        const section = target.href.slice(domainSize);
         if (section) {
             window.history.pushState("", "", section);
             configApp[section].openMethod();
