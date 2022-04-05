7bb3de80f8dfb279f6de999f87212e1a7f0ad40a
Victorius19 <pifagorius2002@gmail.com>
2022-04-05 20:32:08 +0300

    Добавить адекватный конструктор для сторов провиля и базового класса сторов

--- src/stores/baseStore.js
+++ src/stores/baseStore.js
@@ -1,11 +1,30 @@
+'use strict';
+
+import Dispatcher from '../modules/dispatcher.js';
+import EventBus from '../modules/eventBus.js';
+
 /**
  * Базовый класс стора, от которого будут наследоваться все остальные сторы
  */
 class Store {
 	/**
 	 * @constructor
+	 * @param {string} name название события
+	 * @param {object} data состояние стора
 	 */
-	constructor() {
-		this._data = {};
+	constructor(name, data) {
+		this._data = data;
+		this._name = name;
+	}
+
+	/**
+	 * Метод, с помощью которого можно опубликовать события для обновления
+	 * сторов. Она формирует название с названием стора и передает состояние хранилища
+	 * @param {string} event название события
+	 */
+	publish(event) {
+		EventBus.publish(this._name + event, this._data);
 	}
 }
+
+export default new Store();
---
+++ src/stores/profile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import Store from './baseStore.js';
+
+/**
+ * Класс реализующий стор для профиля пользователя
+ */
+class Profile extends Store {
+	/**
+	 * @constructor
+	 * @param {object} data поля состояния стора
+	 */
+	constructor(data) {
+		super('Profile', data);
+	}
+}
