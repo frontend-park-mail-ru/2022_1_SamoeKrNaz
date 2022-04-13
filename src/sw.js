00298ca23da7c5ff15727ada5e8899e5f4afc8e1
yalublukotikov <yalublukotikov@yandex.ru>
2022-04-13 08:17:40 +0300

    поправлен дефолтный урл

--- src/sw.js
+++ src/sw.js
@@ -4,7 +4,7 @@ const timeout = 400;
 self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches.open(cacheName).then((cache) => cache.addAll([
-				'/img/background'
+				'/noNetwork'
 			])
 		));
 });
