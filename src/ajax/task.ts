633389a0abc79590c1ef397b74f4ec8bc826226c
Victorius19 <pifagorius2002@gmail.com>
2022-05-03 22:29:38 +0300

    Добавить модуль для тс

---
+++ src/ajax/task.ts
@@ -0,0 +1,97 @@
+'use strict';
+
+import Ajax from '../ajax/ajax';
+
+export const ajaxMethods = {
+	loadTask: async (data) => {
+		try {
+			return await Ajax.get({url: 'task/' + data.id});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	updateTitle: async (data) => {
+		try {
+			return await Ajax.put({url: 'task/' + data.id, opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	addUser: async (data) => {
+		try {
+			return await Ajax.post({url: 'task/' + data.id + '/' + data.body.idu});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	removeUser: async (data) => {
+		try {
+			return await Ajax.delete({url: 'task/' + data.id + '/' + data.body.idu});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	changeDate: async (data) => {
+		try {
+			return await Ajax.put({url: 'task/' + data.id, opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	changeDescription: async (data) => {
+		try {
+			return await Ajax.put({url: 'task/' + data.id, opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	addChecklist: async (data) => {
+		try {
+			return await Ajax.post({url: 'task/' + data.id + '/checklist', opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	addItem: async (data) => {
+		try {
+			return await Ajax.post({url: 'checklist/' + data.id + '/checklistitem', opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	removeChecklist: async (data) => {
+		try {
+			return await Ajax.delete({url: 'checklist/' + data.id});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	removeItem: async (data) => {
+		try {
+			return await Ajax.delete({url: 'checklistitem/' + data.id});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	checkItem: async (data) => {
+		try {
+			return await Ajax.put({url: 'checklistitem/' + data.id, opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	changeCheckList: async (data) => {
+		try {
+			return await Ajax.put({url: 'checklistitem/' + data.id, opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+	changeCheckListTitle: async (data) => {
+		try {
+			return await Ajax.put({url: 'checklist/' + data.id, opt: JSON.stringify(data.body)});
+		} catch (err) {
+			console.error('Error connection: ' + err);
+		}
+	},
+};
---
+++ src/views/taskView/taskView.ts
@@ -0,0 +1,278 @@
+import taskBlock from '../../components/taskBlock/taskBlock.hbs';
+
+import BaseView from '../baseView';
+import EventBus from '../../modules/eventBus';
+import {BoardActions, Events, TaskActions} from '../../modules/actions';
+import Dispatcher from '../../modules/dispatcher';
+import Board from '../../stores/board';
+import {ProfileStore, TaskStore} from '../../modules/types';
+import Profile from '../../stores/profile';
+
+/**
+ * Класс, реализующий страницу логина.
+ */
+export default new (class TaskView extends BaseView {
+	/**
+	 * @constructor
+	 */
+	constructor() {
+		super([
+			{
+				type: 'click',
+				className: 'taskBlock__close',
+				isArray: true,
+				func: () => {
+					this.close();
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__bg',
+				func: () => {
+					this.close();
+				},
+			},
+			{
+				type: 'input',
+				className: 'taskBlock__title-input',
+				func: (e) => {
+					const hidden = (<HTMLSpanElement>document.querySelector('.taskBlock__hidden'));
+					hidden.innerText = e.target.value;
+					e.target.style.height = hidden.getClientRects()[0].height + 'px';
+				},
+			},
+			{
+				type: 'blur',
+				className: 'taskBlock__title-input',
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.updateTitle,
+						title: e.target.value,
+					});
+				},
+			},
+			{
+				type: 'keydown',
+				className: 'taskBlock__title-input',
+				func: (e) => {
+					if (e.keyCode === 13) {
+						e.target.blur();
+					}
+				},
+			},
+			{
+				type: 'blur',
+				className: 'taskBlock__textarea',
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.changeDescription,
+						data: e.target.value,
+					});
+				},
+			},
+			{
+				type: 'keydown',
+				className: 'taskBlock__textarea',
+				func: (e) => {
+					if (e.keyCode === 13) {
+						e.target.blur();
+					}
+				},
+			},
+			{
+				type: 'blur',
+				className: 'taskBlock__checklist-item-text',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.changeCheckList,
+						title: e.target.value,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'keydown',
+				className: 'taskBlock__checklist-item-text',
+				isArray: true,
+				func: (e) => {
+					if (e.keyCode === 13) {
+						e.target.blur();
+					}
+				},
+			},
+			{
+				type: 'blur',
+				className: 'taskBlock__checklist-title',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.changeCheckListTitle,
+						title: e.target.value,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'keydown',
+				className: 'taskBlock__checklist-title',
+				isArray: true,
+				func: (e) => {
+					if (e.keyCode === 13) {
+						e.target.blur();
+					}
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__user',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.addUser,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__img-block-delete',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.removeUser,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__button',
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.addChecklist,
+					});
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__button-active',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.addItem,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__checklist-delete',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.removeChecklist,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__item-delete',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.removeItem,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'click',
+				className: 'taskBlock__item-check',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.checkItem,
+						id: e.target.dataset.id,
+					});
+				},
+			},
+			{
+				type: 'change',
+				className: 'taskBlock__date',
+				isArray: true,
+				func: (e) => {
+					Dispatcher.dispatch({
+						type: TaskActions.changeDate,
+						data: e.target.value,
+					});
+				},
+			},
+		]);
+
+		EventBus.subscribe(Events.taskUpdate, this.onUpdate.bind(this));
+	}
+
+	/**
+	 * Метод отвечающий за генерацию View
+	 * @param {object} e
+	 */
+	render(e): void {
+		const block = document.querySelector('.taskBlockContainer');
+		block.innerHTML = taskBlock();
+		block.classList.toggle('taskBlock_active');
+
+		this._createListeners();
+
+		Dispatcher.dispatch({
+			type: TaskActions.loadTask,
+			data: e.target.dataset.id,
+		});
+	}
+
+	/**
+	 * Метод, вызывающийся при обновлении стора досок
+	 * @param {TaskStore} data состояние стора
+	 */
+	onUpdate(data: TaskStore = null): void {
+		this.removeListeners();
+
+		const addUsers = Board.getUsers().slice(0);
+
+		addUsers.forEach((user, i) => {
+			data.append_users.map((userTwo) => {
+				if (userTwo.idu === user.idu) {
+					delete addUsers[i];
+				}
+			});
+		});
+
+		const block = document.querySelector('.taskBlockContainer');
+		block.innerHTML = taskBlock({
+			...data,
+			deskTitle: Board.getTitle(),
+			addUsers: addUsers,
+			user: Profile.getState(),
+		});
+
+		const textarea = (<HTMLTextAreaElement>document.querySelector('.taskBlock__title-input'));
+		const hidden = (<HTMLSpanElement>document.querySelector('.taskBlock__hidden'));
+		hidden.innerText = textarea.value;
+		textarea.style.height = hidden.getClientRects()[0].height + 'px';
+
+		this._createListeners();
+	}
+
+	/**
+	 * Закрыть таску
+	 */
+	close() {
+		const block = document.querySelector('.taskBlockContainer');
+		block.classList.toggle('taskBlock_active');
+
+		Dispatcher.dispatch({
+			type: BoardActions.loadBoard,
+		});
+	}
+});
