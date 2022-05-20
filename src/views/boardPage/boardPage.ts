ffaad7264eebe63ee4e9d0a2d44e71c0b62b1517
Victor <pifagorius2002@gmail.com>
2022-05-20 15:54:56 +0300

    Можно тоскать колонки

--- src/views/boardPage/boardPage.ts
+++ src/views/boardPage/boardPage.ts
@@ -10,6 +10,7 @@ import {BoardActions, Events} from '../../modules/actions';
 import Dispatcher from '../../modules/dispatcher';
 import Board from '../../stores/board';
 import {Event} from '../../modules/types';
+import {dndEvent} from '../../modules/dndEvent';
 
 type ModalSettings = {
 	type: string,
@@ -263,6 +264,7 @@ export default new (class BoardPage extends BaseView {
 		/* Добавление контента в DOM */
 		root.innerHTML = html;
 
+		this.addDNDEvents();
 		this._createListeners();
 	}
 
@@ -347,6 +349,19 @@ export default new (class BoardPage extends BaseView {
 			body: payload,
 			id: (e.path[0].dataset.id) ? e.path[0].dataset.id : null,
 		});
+	}
+
+	/**
+	 * Добавляет events на нужные элементы для drag and drop
+	 */
+	addDNDEvents(): void {
+		const columns = document.querySelectorAll('.desk__column');
+
+		for (const key in columns) {
+			if (columns.hasOwnProperty(key)) {
+				dndEvent((<HTMLElement> columns[key]), columns[key].querySelector('.desk__column-title'));
+			}
+		}
 	}
 });
 
