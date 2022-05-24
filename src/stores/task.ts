fc6d31c90fed7e81875b3a9ccfe685986c78e904
yalublukotikov <92160117+yalublukotikov@users.noreply.github.com>
2022-05-24 23:18:28 +0300

    4.228: Фиксы (#22)
    * фиксы
    
    * я идиот 2
    
    * я идиот 3
    
    * работает

--- src/stores/task.ts
+++ src/stores/task.ts
@@ -378,7 +378,6 @@ class Task extends Store {
 		const res = await ajaxMethods.uploadAttachment({id: this._data.idt, opt: formData});
 		switch (res.status) {
 		case ResponseStatus.success:
-			res.body.system_name = '../attachments/' + res.body.system_name;
 			this._data.attachments.push(res.body);
 			break;
 		}
@@ -413,7 +412,7 @@ class Task extends Store {
 		const attachment = this._data.attachments.find((attach) => {
 			return attach.id_a === Number(action.id);
 		});
-		window.open(attachment.system_name, attachment.default_name);
+		window.open('../' + attachment.system_name + '?name=' + attachment.default_name, `Download: ${attachment.default_name}`);
 	}
 
 	/**
