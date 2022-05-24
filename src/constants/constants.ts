ac41533a6ed6e1816d2bbced8c9db6bf6d17ef45
yalublukotikov <yalublukotikov@yandex.ru>
2022-05-24 21:42:07 +0300

    я идиот 3

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -41,5 +41,5 @@ export const Sizes = {
 	xl: 1200,
 };
 
-export const backendUrl = window.location.origin.replace(':3000', ':8080') + '/';
+export const backendUrl = window.location.origin.replace(':3000', ':8080') + '/api/';
 export const frontendUrl = window.location.origin;
